11962f5c2aec7eb322520114342128efaf616e2a
taroun <aksghk4671@gmail.com>
2021-12-09 13:52:42 +0900

    12/09 hello world template update

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, hello_world_template
 
 app_name = 'accountapp'
 
 urlpatterns = [
+    path('hello_world_template/', hello_world_template, name='hello_world_template'),
     path('hello_world/', hello_world, name='hello_world'),
 ]
+
--- accountapp/views.py
+++ accountapp/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
+def hello_world_template(request):
+    return render(request, 'accountapp/hello_world.html')
+
+
 @api_view()
 def hello_world(request):
     return Response({'message': 'Hello World!'})
