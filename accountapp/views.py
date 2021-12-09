c51e38b5d960a414f834be4578a0de8e85ece56b
taroun <aksghk4671@gmail.com>
2021-12-09 15:49:51 +0900

    12/09 axios http post update

--- accountapp/views.py
+++ accountapp/views.py
@@ -9,7 +9,12 @@ def hello_world_template(request):
     return render(request, 'accountapp/hello_world.html')
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def hello_world(request):
+
+    if request.method == 'POST':
+        input_data = request.data.get('input_data')
+        return Response({'message': input_data})
+
     return Response({'message': 'Return Text!'})
 
