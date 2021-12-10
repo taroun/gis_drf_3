cc9803aec437da61e7cd169cfce3e0d523c3cdb6
taroun <aksghk4671@gmail.com>
2021-12-10 10:30:41 +0900

    12/10 development static file serving update

---
+++ accountapp/static/js/hello_world.js
@@ -0,0 +1,26 @@
+    function send_input() {
+        axios.post('/accounts/hello_world/', {
+            input_data: document.getElementById('input_data').value,
+          })
+          .then(function (response) {
+            console.log(response);
+            document.getElementById('text').innerHTML = response.data['message'];
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
+    axios.get('/accounts/hello_world/')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        // 성공 했을 경우 어떤 javascript 작업을 할지
+        document.getElementById('text').innerHTML = response.data['message']; // Return Text!
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
