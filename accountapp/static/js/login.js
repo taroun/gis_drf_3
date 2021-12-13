a1aa8551224615d90477f550f4bd56855c20b9a4
aksgh <aksghk4671@gmail.com>
2021-12-13 15:53:55 +0900

    12/13 logout update

--- accountapp/static/js/login.js
+++ accountapp/static/js/login.js
@@ -32,9 +32,6 @@ function send_input() {
             // 성공했을 경우
             console.log(response);
 
-            document.getElementById('alert_box').innerHTML
-                = "<div class='btn btn-primary rounded-pill px-5'>로그인이 성공했습니다</div>";
-
             // Token 수령 후 쿠키 생성
             setCookie('drf_token','Token ' + response.data['token'])
 
