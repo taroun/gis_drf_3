05f93a2be07675d172bbe60b0b0edd3241777828
aksgh <aksghk4671@gmail.com>
2021-12-14 15:46:37 +0900

    12/14 update API error fixed

--- accountapp/static/js/update.js
+++ accountapp/static/js/update.js
@@ -1,35 +1,42 @@
 function initialize (pk) {
     axios.get('/accounts/retrieve/' + pk )
-      .then(function (response) {
-        // handle success
-        console.log(response);
+        .then(function (response) {
+            // handle success
+            console.log(response);
 
-        document.getElementById('username').value = response.data['username'];
-        document.getElementById('email').value = response.data['email'];
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-      });
+            document.getElementById('username').value = response.data['username'];
+            document.getElementById('email').value = response.data['email'];
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
 }
 
 function update_account(pk) {
-    axios.patch('/accounts/update/' + pk , {
-        username: document.getElementById('username').value,
-        email: document.getElementById('email').value,
+    axios({
+        method: 'patch',
+        url: '/accounts/update/' + pk,
+        data: {
+            username: document.getElementById('username').value,
+            email: document.getElementById('email').value,
+        },
+        headers: {
+            Authorization: decodeURIComponent(getCookie('drf_token')),
+        }
+        .then(function (response) {
+            // handle success
+            console.log(response);
+            window.location.href = '/accounts/retrieve_template/' + pk ;
+        })
     })
-      .then(function (response) {
-        // handle success
-        console.log(response);
-        window.location.href = '/accounts/retrieve_template/' + pk ;
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-        if (error.response.status === 401) {
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+            if (error.response.status === 401) {
                 document.getElementById('alert_box').innerHTML
                     = "<div class='btn btn-danger rounded-pill px-5'>인증 정보가 없습니다.</div>"
             } else if (error.response.status === 403) {
@@ -39,8 +46,8 @@ function update_account(pk) {
                 document.getElementById('alert_box').innerHTML
                     = "<div class='btn btn-danger rounded-pill px-5'>업데이트에 실패했습니다.</div>"
             }
-      })
-      .then(function () {
-        // always executed
-      });
+        })
+        .then(function () {
+            // always executed
+        });
 }
