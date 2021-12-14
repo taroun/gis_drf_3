a9182747ccddebfbb9de30d9b792b0254c912994
aksgh <aksghk4671@gmail.com>
2021-12-14 09:33:14 +0900

    12/14 Retrieve API template update2

--- accountapp/static/js/retrieve.js
+++ accountapp/static/js/retrieve.js
@@ -1,14 +1,18 @@
 
+function initialize (pk) {
+    axios.get('/accounts/retrieve/' + pk )
+      .then(function (response) {
+        // handle success
+        console.log(response);
 
-axios.get('/accounts/retrieve/{{ request.GET.get("pk") }}')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
+        document.getElementById('username').innerHTML = response.data['username'];
+        document.getElementById('date_joined').innerHTML = response.data['date_joined'];
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+}
