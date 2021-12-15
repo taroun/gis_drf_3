4c03e9772622b4db28d751ef48b9b67daacf2b6e
aksgh <aksghk4671@gmail.com>
2021-12-15 22:28:49 +0900

    12/15 Profileapp update

--- gis_drf_3/urls.py
+++ gis_drf_3/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('accounts/', include('accountapp.urls')),
+    path('profiles/', include('profileapp.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
