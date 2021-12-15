09c2be0475b4e4d2b861e96bf9bd62e7d9b595d7
aksgh <aksghk4671@gmail.com>
2021-12-15 23:07:37 +0900

    12/15 profile create template redirect update

--- profileapp/serializers.py
+++ profileapp/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework.serializers import ModelSerializer
 
+from accountapp.serializers import UserWithoutPasswordSerializer
 from profileapp.models import Profile
 
 
 class ProfileSerializer(ModelSerializer):
+    owner = UserWithoutPasswordSerializer(read_only=True)
+
     class Meta:
         model = Profile
-        fields = ['id', 'nickname', 'image', 'message']
+        fields = ['id', 'nickname', 'image', 'message', 'owner']
 
