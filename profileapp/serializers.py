8430cf79162cb7ffbd076cae4917d8dfbe603f31
aksgh <aksghk4671@gmail.com>
2021-12-16 15:36:44 +0900

    12/16 thumbnail generation update

--- profileapp/serializers.py
+++ profileapp/serializers.py
@@ -6,5 +6,5 @@ from profileapp.models import Profile
 class ProfileSerializer(ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['id', 'nickname', 'image', 'message', 'owner_id']
+        fields = ['id', 'nickname', 'image', 'thumb', 'message', 'owner_id']
 
