f12e2e684e6cc29380a8efff828135e0fddc0c2b
aksgh <aksghk4671@gmail.com>
2021-12-16 15:57:45 +0900

    12/16 thumbnail img src update

--- accountapp/static/accountapp/js/retrieve.js
+++ accountapp/static/accountapp/js/retrieve.js
@@ -1,20 +1,29 @@
 
 function initialize (pk) {
     axios.get('/accounts/' + pk )
-      .then(function (response) {
-        // handle success
-        console.log(response);
+        .then(function (response) {
+            // handle success
+            console.log(response);
 
-        document.getElementById('nickname').innerHTML = response.data['profile']['nickname'];
-        document.getElementById('message').innerHTML = response.data['profile']['message'];
+            document.getElementById('nickname').innerHTML = response.data['profile']['nickname'];
+            document.getElementById('message').innerHTML = response.data['profile']['message'];
 
-        document.getElementById('image').src = response.data['profile']['image'];
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-      });
+            if (response.data['profile']['thumb'] !== null) {
+                document.getElementById('image').src = response.data['profile']['thumb'];
+            } else if (response.data['profile']['image'] !== null) {
+                document.getElementById('image').src = response.data['profile']['image'];
+            } else {
+                document.getElementById('image').style.display = none;
+            }
+
+
+
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
 }
