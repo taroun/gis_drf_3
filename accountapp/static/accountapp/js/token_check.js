841788e3c08dd105a4fdb7beea0609a745f2ccda
aksgh <aksghk4671@gmail.com>
2021-12-16 13:54:29 +0900

    12/16 header button error fixed

--- accountapp/static/accountapp/js/token_check.js
+++ accountapp/static/accountapp/js/token_check.js
@@ -5,30 +5,30 @@ function getCookie(name) {
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
-var user_id;
-var check = getCookie('drf_token');
 
-axios({
-    method: 'get',
-    url: '/accounts/token/',
-    headers: {
-        Authorization: decodeURIComponent(getCookie('drf_token'))
-    }
-})
-    .then(function (response) {
-        // 성공했을 경우
-        user_id = response.data['id'];
-        if (check !== undefined) {
-            document.getElementById('signup_button').innerHTML =
-            "<a href=\"/accounts/retrieve_template/" + user_id + "\">\n" +
-            "                MyPage\n" +
-            "            </a>";
-        }
-    })
+var check = getCookie('drf_token');
 
 if (check !== undefined) {
     document.getElementById('login_button').innerHTML =
         "<a href=\"/accounts/logout_template/\">\n" +
         "                Logout\n" +
         "            </a>";
+
+    var user_id;
+
+    axios({
+        method: 'get',
+        url: '/accounts/token/',
+        headers: {
+            Authorization: decodeURIComponent(getCookie('drf_token'))
+        }
+    })
+    .then(function (response) {
+        // 성공했을 경우
+        user_id = response.data['id'];
+        document.getElementById('signup_button').innerHTML =
+        "<a href=\"/accounts/retrieve_template/" + user_id + "\">\n" +
+        "                MyPage\n" +
+        "            </a>";
+    })
 }
