b72a32635e87c845e4f5d2e5dcc2f460379c4c39
aksgh <aksghk4671@gmail.com>
2021-12-16 09:44:33 +0900

    12/16 profile template update

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from profileapp.views import ProfileCreateAPIView, ProfileCreateTemplateView, ProfileUpdateAPIView
+from profileapp.views import ProfileCreateAPIView, ProfileCreateTemplateView, ProfileUpdateAPIView, \
+    ProfileUpdateTemplateView
 
 urlpatterns = [
     path('create_template/', ProfileCreateTemplateView.as_view(), name='create_template'),
+    path('update_template/<int:pk>', ProfileUpdateTemplateView.as_view(), name='update_template'),
 
     path('', ProfileCreateAPIView.as_view(), name='create'),
     path('<int:pk>', ProfileUpdateAPIView.as_view(), name='update'),
