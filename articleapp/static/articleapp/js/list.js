ac6a8ed9587171fef556323fbadeb37009eaa994
aksgh <aksghk4671@gmail.com>
2021-12-22 13:49:52 +0900

    12/22 article pagination template update

--- articleapp/static/articleapp/js/list.js
+++ articleapp/static/articleapp/js/list.js
@@ -8,7 +8,7 @@ function getCookie(name) {
 function initialize() {
     axios({
         method: 'get',
-        url: '/articles/list/',
+        url: '/articles/',
         headers: {
             Authorization: decodeURIComponent(getCookie('drf_token')),
         }
@@ -18,18 +18,39 @@ function initialize() {
             console.log(response);
 
             for (let i=0; i < response.data['results'].length; i++) {
-                document.getElementById('article_list').innerHTML
+                document.getElementById('item' + i ).innerHTML
                     +=
-                    "<div>" +
                     "<a href=\"/articles/retrieve_template/" + response.data['results'][i]['id'] + "\">" +
                     "<img style=\"width: 100%; border-radius: 1rem;\"" +
                     " src=\"" + response.data['results'][i]['image'] + "\"" +
                     " alt=\"\">" +
-                    "</a>\"" +
-                    "</div>";
+                    "</a>";
             }
 
             // paginator 기반 page 객체 생성
+            var pagination = document.getElementById('pagination');
+
+            if (response.data['previous'] !== null) {
+                pagination.innerHTML +=
+                    "<a href=\"" + response.data['previous'] + "\"" +
+                    "   class=\"btn btn-secondary rounded-pill px-5 mx-3\">" +
+                    "Previous" +
+                    "</a>";
+            }
+
+            pagination.innerHTML +=
+                    "<a href=\"#\"" +
+                    "   class=\"btn btn-dark rounded-pill px-5 mx-3\">" +
+                    "Current page" +
+                    "</a>";
+
+            if (response.data['next'] !== null) {
+                pagination.innerHTML +=
+                    "<a href=\"" + response.data['next'] + "\"" +
+                    "   class=\"btn btn-secondary rounded-pill px-5 mx-3\">" +
+                    "Next" +
+                    "</a>";
+            }
 
         })
         .catch(function (error) {
